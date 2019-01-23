8874e0c7add023be452ada1e7b4eea2a4401e48a
nmarklund10 <nmarklund10@outlook.com>
2019-01-23 20:03:57 +0000

    generate proper counter for aes

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,22 @@
+function generateCounter() {
+  var ctrPart = new Uint32Array(4);
+  window.crypto.getRandomValues(ctrPart);
+  var ctr = BigInt(ctrPart[0]) << BigInt(96);
+  ctr |= (BigInt(ctrPart[1]) << BigInt(64));
+  ctr |= (BigInt(ctrPart[2]) << BigInt(32));
+  ctr |= BigInt(ctrPart[3]);
+  ctr = ctr.toString(16);
+  return aesjs.utils.hex.toBytes(ctr);
+}
+
+function decryptVault() {
+  var encryptedVault = aesjs.utils.hex.toBytes(window.sessionStorage.vault);
+  var key = aesjs.utils.hex.toBytes(window.sessionStorage["decKey"]);
+  var ctr = generateCounter();
+  var aes = new aesjs.ModeOfOperation.cbc(key, ctr);
+  var decryptedVault = aes.decrypt(encryptedVault);
+}
+
 function displayVault() {
   if ('vault' in window.sessionStorage) {
     document.getElementById('message').innerText = 'Your Stuff: ' + window.sessionStorage.vault;
