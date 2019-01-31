d0bff0dd661d3d0d31af88e5526d83d9f6c3ed8c
Neil Marklund <nmarklund10@hotmail.com>
2019-01-30 20:55:21 -0500

    trimmed input values

--- public/js/login.js
+++ public/js/login.js
@@ -14,38 +14,38 @@ function setupEnterKeyListeners() {
 }
 
 function login() {
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
+  var username = document.getElementById('username').value.trim();
+  var password = document.getElementById('password').value.trim();
   if (username == '') {
     document.getElementById('error').innerText = '⚠️ Username field required!';
-    return;
   }
-  if (password == '') {
+  else if (password == '') {
     document.getElementById('error').innerText = '⚠️ Password field required!';
-    return;
   }
-  document.getElementById('error').innerText = '';
-  var hash = sha256(username + password);
-  for (var i = 0; i < 4999; i++) {
+  else {
+    document.getElementById('error').innerText = '';
+    var hash = sha256(username + password);
+    for (var i = 0; i < 4999; i++) {
+      hash = sha256(hash);
+    }
+    window.sessionStorage['decKey'] = hash;
     hash = sha256(hash);
+    var userInfo = {
+      username: username,
+      hash: hash,
+    };
+    sendPostRequest('/login', userInfo, function(response) {
+      if (response.success) {
+        sendPostRequest('/home', {token: response.token}, function(secondResponse) {
+          window.sessionStorage['vault'] = secondResponse.vault;
+          window.sessionStorage['ctr'] = secondResponse.ctr;
+          window.sessionStorage['user'] = username;
+          window.location = '/dashboard';
+        });
+      }
+      else {
+        document.getElementById('error').innerText = '⚠️ ' + response.message;
+      }
+    });
   }
-  window.sessionStorage['decKey'] = hash;
-  hash = sha256(hash);
-  var userInfo = {
-    username: username,
-    hash: hash,
-  };
-  sendPostRequest('/login', userInfo, function(response) {
-    if (response.success) {
-      sendPostRequest('/home', {token: response.token}, function(secondResponse) {
-        window.sessionStorage['vault'] = secondResponse.vault;
-        window.sessionStorage['ctr'] = secondResponse.ctr;
-        window.sessionStorage['user'] = username;
-        window.location = '/dashboard';
-      });
-    }
-    else {
-      document.getElementById('error').innerText = '⚠️ ' + response.message;
-    }
-  });
 }
--- public/js/signUp.js
+++ public/js/signUp.js
@@ -19,51 +19,48 @@ function setupEnterKeyListeners() {
 }
 
 function newUser() {
-  var username = document.getElementById('username').value;
-  var password = document.getElementsByClassName('password')[0].value;
-  var passwordCheck = document.getElementsByClassName('password')[1].value;
+  var username = document.getElementById('username').value.trim();
+  var password = document.getElementsByClassName('password')[0].value.trim();
+  var passwordCheck = document.getElementsByClassName('password')[1].value.trim();
   if (username == '') {
     document.getElementById('error').innerText = '⚠️ Username field required!';
-    return;
   }
-  if (username.length < 6 || username.length > 20) {
+  else if (username.length < 6 || username.length > 20) {
     document.getElementById('error').innerText = '⚠️ Username must be 6-20 characters long!';
-    return;
   }
-  if (password == '') {
+  else if (password == '') {
     document.getElementById('error').innerText = '⚠️ Password field required!';
-    return;
   }
-  if (password.length < 12 || password.length > 64) {
+  else if (password.length < 12 || password.length > 64) {
     document.getElementById('error').innerText = '⚠️ Password must be 12-64 characters long!';
-    return;
   }
-  if (password != passwordCheck) {
+  else if (password != passwordCheck) {
     document.getElementById('error').innerText = '⚠️ Passwords do not match!';
-    return;
   }
-  document.getElementById('error').innerText = '';
-  var hash = sha256(username + password);
-  for (var i = 0; i < 4999; i++) {
+  else {
+    document.getElementById('error').innerText = '';
+    var hash = sha256(username + password);
+    for (var i = 0; i < 4999; i++) {
+      hash = sha256(hash);
+    }
+    window.sessionStorage['decKey'] = hash;
     hash = sha256(hash);
+    var userInfo = {
+      username: username,
+      hash: hash,
+    };
+    sendPostRequest('/newUser', userInfo, function(response) {
+      if (response.success) {
+        sendPostRequest('/home', {token: response.token}, function(secondResponse) {
+          window.sessionStorage['vault'] = secondResponse.vault;
+          window.sessionStorage['ctr'] = secondResponse.ctr;
+          window.sessionStorage['user'] = username;
+          window.location = '/dashboard';
+        });
+      }
+      else {
+        document.getElementById('error').innerText = '⚠️ ' + response.message;
+      }
+    });
   }
-  window.sessionStorage['decKey'] = hash;
-  hash = sha256(hash);
-  var userInfo = {
-    username: username,
-    hash: hash,
-  };
-  sendPostRequest('/newUser', userInfo, function(response) {
-    if (response.success) {
-      sendPostRequest('/home', {token: response.token}, function(secondResponse) {
-        window.sessionStorage['vault'] = secondResponse.vault;
-        window.sessionStorage['ctr'] = secondResponse.ctr;
-        window.sessionStorage['user'] = username;
-        window.location = '/dashboard';
-      });
-    }
-    else {
-      document.getElementById('error').innerText = '⚠️ ' + response.message;
-    }
-  });
 }
